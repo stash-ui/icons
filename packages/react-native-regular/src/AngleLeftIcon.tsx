31bb9a307d9311f9c15b8678edba81d8c346f71f
Pedro Ladeira <pmladeira@live.com>
2022-12-20 11:12:39 -0300

    package(react-native-regular): change return

--- packages/react-native-regular/src/AngleLeftIcon.tsx
+++ packages/react-native-regular/src/AngleLeftIcon.tsx
@@ -2,15 +2,13 @@ import * as React from 'react';
 import Svg, { Path } from 'react-native-svg';
 import { IconProps } from './types';
 
-export const AngleLeftIcon = ({ color = 'currentColor', ...props }) => {
-  return (
-    <Svg width="24" height="24" viewBox="0 0 24 24" fill="none" {...props}>
-      <Path
-        d="M13.8364 8.9636C14.1879 9.31507 14.1879 9.88492 13.8364 10.2364L12.0728 12L13.8364 13.7636C14.1879 14.1151 14.1879 14.6849 13.8364 15.0364C13.4849 15.3879 12.9151 15.3879 12.5636 15.0364L10.1636 12.6364C9.81216 12.2849 9.81216 11.7151 10.1636 11.3636L12.5636 8.9636C12.9151 8.61213 13.4849 8.61213 13.8364 8.9636Z"
-        fill={color}
-      />
-    </Svg>
-  );
-};
+export const AngleLeftIcon = ({ color = 'currentColor', ...props }) => (
+  <Svg width="24" height="24" viewBox="0 0 24 24" fill="none" {...props}>
+    <Path
+      d="M13.8364 8.9636C14.1879 9.31507 14.1879 9.88492 13.8364 10.2364L12.0728 12L13.8364 13.7636C14.1879 14.1151 14.1879 14.6849 13.8364 15.0364C13.4849 15.3879 12.9151 15.3879 12.5636 15.0364L10.1636 12.6364C9.81216 12.2849 9.81216 11.7151 10.1636 11.3636L12.5636 8.9636C12.9151 8.61213 13.4849 8.61213 13.8364 8.9636Z"
+      fill={color}
+    />
+  </Svg>
+);
 
 export default AngleLeftIcon;
--- packages/react-native-regular/src/AngleUpIcon.tsx
+++ packages/react-native-regular/src/AngleUpIcon.tsx
@@ -2,15 +2,13 @@ import * as React from 'react';
 import Svg, { Path } from 'react-native-svg';
 import { IconProps } from './types';
 
-export const AngleUpIcon = ({ color = 'currentColor', ...props }) => {
-  return (
-    <Svg width="24" height="24" viewBox="0 0 24 24" fill="none" {...props}>
-      <Path
-        d="M11.3636 10.1636C11.7151 9.81213 12.2849 9.81213 12.6364 10.1636L15.0364 12.5636C15.3879 12.9151 15.3879 13.4849 15.0364 13.8364C14.6849 14.1879 14.1151 14.1879 13.7636 13.8364L12 12.0728L10.2364 13.8364C9.88494 14.1879 9.31509 14.1879 8.96362 13.8364C8.61214 13.4849 8.61214 12.9151 8.96362 12.5636L11.3636 10.1636Z"
-        fill={color}
-      />
-    </Svg>
-  );
-};
+export const AngleUpIcon = ({ color = 'currentColor', ...props }) => (
+  <Svg width="24" height="24" viewBox="0 0 24 24" fill="none" {...props}>
+    <Path
+      d="M11.3636 10.1636C11.7151 9.81213 12.2849 9.81213 12.6364 10.1636L15.0364 12.5636C15.3879 12.9151 15.3879 13.4849 15.0364 13.8364C14.6849 14.1879 14.1151 14.1879 13.7636 13.8364L12 12.0728L10.2364 13.8364C9.88494 14.1879 9.31509 14.1879 8.96362 13.8364C8.61214 13.4849 8.61214 12.9151 8.96362 12.5636L11.3636 10.1636Z"
+      fill={color}
+    />
+  </Svg>
+);
 
 export default AngleUpIcon;
