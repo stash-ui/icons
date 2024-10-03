4422b0ae39df5d1580048b727792efa79a041ea7
Pedro Ladeira <pmladeira@live.com>
2024-10-02 22:10:42 -0300

    feat(react-native-regular): new icons

--- packages/react-native-regular/src/AngleRightIcon.tsx
+++ packages/react-native-regular/src/AngleRightIcon.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import Svg, { Path } from 'react-native-svg';
-import { IconProps } from './types';
 
 export const AngleRightIcon = ({ color = 'currentColor', ...props }) => (
   <Svg width="24" height="24" viewBox="0 0 24 24" fill="none" {...props}>
