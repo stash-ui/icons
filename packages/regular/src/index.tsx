d0da75a1b805a85fad82048d2ea97f2443505b4f
Pedro Ladeira <pmladeira@live.com>
2024-10-04 15:36:38 -0300

    feat: UserUpdate icon

--- packages/regular/src/index.tsx
+++ packages/regular/src/index.tsx
@@ -291,3 +291,4 @@ export { default as SpinnerIcon } from './SpinnerIcon';
 export { default as SidebarIcon } from './SidebarIcon';
 export { default as ListRemovedIcon } from './ListRemovedIcon';
 export { default as KanbanIcon } from './KanbanIcon';
+export { default as UserUpdateIcon } from './UserUpdateIcon';
