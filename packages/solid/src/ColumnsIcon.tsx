21b3d1fd87ae4dbff523cff7d4d2e751d93cfcf3
Jéss Martins <81935519+Jessmartins91@users.noreply.github.com>
2025-04-09 17:24:10 -0300

    fix: changes requested

--- packages/solid/src/ColumnsIcon.tsx
+++ packages/solid/src/ColumnsIcon.tsx
@@ -17,7 +17,7 @@ export const ColumnsIcon = React.forwardRef<SVGSVGElement, IconProps>(
           d="M16 19.25H13.5L13.5 4.75H16C17.2426 4.75 18.25 5.75736 18.25 7V17C18.25 18.2426 17.2426 19.25 16 19.25ZM8 4.75H10.5L10.5 19.25H8C6.75736 19.25 5.75 18.2426 5.75 17V7C5.75 5.75736 6.75736 4.75 8 4.75Z"
           fill={color}
           stroke={color}
-          stroke-width="1.5"
+          strokeWidth="1.5"
         />
       </svg>
     );
